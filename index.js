916164b0986fa2626e5eb7f8a43a849cafc8cced
Bruno Amaral <brunoamarallh@gmail.com>
2023-12-04 20:35:33 -0300

    taboca

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ function soma (a, b) {
   return a + b;
 }
 
-const resultado = soma(5, 9);
-
-
-console.log("resultado", resultado)
+const resultado = soma(10, 6);
 
 
 console.log("coe familia")
+
+console.log('tabica')
